1f74a0c7e8739891aa726cfbe6222c0288f630be
Stimicc <36703755+Stimicc@users.noreply.github.com>
2018-02-21 17:42:00 +0100

    1_alfa
    Pierwszy commit: uploadowane index.php i style.css

---
+++ fizyka/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html hreflang="pl"> <!--ADD LANGUAGE DESCRPTION IN GOOGLE-->
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>Fizyka - wzory</title>
+	<meta name="description" content="Fizyka - wzory dla każdego"/>
+	<meta name="keywords" content="fizyka, kozłowska, wzory, prędkość kosmiczna"/> <!--DOKOŃCZYĆ!!!!!-->
+	<meta name="Author" content="Jakub Miros, Michał Wójtowicz, Mikołaj Wtulich"/>
+	<link href="assets/style.css" rel="stylesheet" type="text/css"/>	
+</head>
+<body>
+	<div class="main_title">
+		<h1>Wzory z fizyki dla każdego :)</h1>
+		<h3>TU WSTAW OPIS</h3>
+		<h2>Wybierz dział fizyki:</h2>
+	</div>
+	<div class="main_choose">
+		<form method="POST" action="XD">
+			<input id="choose-input" type="text" name="choose" class="input_choose" placeholder="Wybierz dział:">
+		</form>
+	</div>
+	<div style="clear:both;"></div>
+	<div class="footer">2018&copy <a>Autorzy: Michał Wójtowicz, Mikołaj Wtulich, Jakub Miros</a></div>	
+</body>
+</html>
